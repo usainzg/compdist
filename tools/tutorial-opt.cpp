ab1fb1eea0477ad0983eed3979bec60e1d3f263c
Unai Sainz de la Maza <unaihtc70@gmail.com>
2024-10-29 09:25:17 +0100

    clean tutorial code

--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -1,9 +1,4 @@
-#include "lib/Conversion/PolyToStandard/PolyToStandard.h"
-#include "lib/Dialect/Noisy/NoisyDialect.h"
-#include "lib/Dialect/Poly/PolyDialect.h"
 #include "lib/Transform/Affine/Passes.h"
-#include "lib/Transform/Arith/Passes.h"
-#include "lib/Transform/Noisy/Passes.h"
 #include "mlir/include/mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
 #include "mlir/include/mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
 #include "mlir/include/mlir/Conversion/FuncToLLVM/ConvertFuncToLLVMPass.h"
@@ -19,60 +14,12 @@
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/include/mlir/Transforms/Passes.h"
 
-void polyToLLVMPipelineBuilder(mlir::OpPassManager &manager) {
-  // Poly
-  manager.addPass(mlir::tutorial::poly::createPolyToStandard());
-  manager.addPass(mlir::createCanonicalizerPass());
-
-  manager.addPass(mlir::createConvertElementwiseToLinalgPass());
-  manager.addPass(mlir::createConvertTensorToLinalgPass());
-
-  // One-shot bufferize, from
-  // https://mlir.llvm.org/docs/Bufferization/#ownership-based-buffer-deallocation
-  mlir::bufferization::OneShotBufferizationOptions bufferizationOptions;
-  bufferizationOptions.bufferizeFunctionBoundaries = true;
-  manager.addPass(
-      mlir::bufferization::createOneShotBufferizePass(bufferizationOptions));
-  mlir::bufferization::BufferDeallocationPipelineOptions deallocationOptions;
-  mlir::bufferization::buildBufferDeallocationPipeline(manager,
-                                                       deallocationOptions);
-
-  manager.addPass(mlir::createConvertLinalgToLoopsPass());
-
-  // Needed to lower memref.subview
-  manager.addPass(mlir::memref::createExpandStridedMetadataPass());
-
-  manager.addPass(mlir::createConvertSCFToCFPass());
-  manager.addPass(mlir::createConvertControlFlowToLLVMPass());
-  manager.addPass(mlir::createArithToLLVMConversionPass());
-  manager.addPass(mlir::createConvertFuncToLLVMPass());
-  manager.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
-  manager.addPass(mlir::createReconcileUnrealizedCastsPass());
-
-  // Cleanup
-  manager.addPass(mlir::createCanonicalizerPass());
-  manager.addPass(mlir::createSCCPPass());
-  manager.addPass(mlir::createCSEPass());
-  manager.addPass(mlir::createSymbolDCEPass());
-}
-
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
-  registry.insert<mlir::tutorial::poly::PolyDialect>();
-  registry.insert<mlir::tutorial::noisy::NoisyDialect>();
   mlir::registerAllDialects(registry);
   mlir::registerAllPasses();
 
   mlir::tutorial::registerAffinePasses();
-  mlir::tutorial::registerArithPasses();
-  mlir::tutorial::noisy::registerNoisyPasses();
-
-  // Dialect conversion passes
-  mlir::tutorial::poly::registerPolyToStandardPasses();
-
-  mlir::PassPipelineRegistration<>(
-      "poly-to-llvm", "Run passes to lower the poly dialect to LLVM",
-      polyToLLVMPipelineBuilder);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
